9ce36ccdb240abb8771b0e80129d0806fac9ebb0
Alexander Gittings <Alexander.Gittings@imtechictuk.lan>
2019-05-03 17:08:47 +0100

    add more commands

--- sp_cli.py
+++ sp_cli.py
@@ -111,3 +111,148 @@ def get_appliance(devices, ec):
         result = api.get_appliance(ec).data
         outFormat(device.table, result)
         click.secho("Task Completed")
+
+@main.command('get_reach_app')
+@click.argument('ec')
+@click.pass_obj
+def get_reach_app(devices, ec):
+    """Get the reachability status from the appliance"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_reach_app(ec).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_reach_gms')
+@click.argument('ec')
+@click.pass_obj
+def get_reach_gms(devices, ec):
+    """Get the reachability status from the orchestrator"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_reach_gms(ec).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_groups')
+@click.pass_obj
+def get_groups(devices):
+    """Get all orchestrator groups"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_groups().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_group')
+@click.argument('id')
+@click.pass_obj
+def get_group(devices, id):
+    """Get a sigle group from orchestrator"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_group(id).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_group_root')
+@click.pass_obj
+def get_group_root(devices):
+    """Get root group"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_group_root().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_grnodes')
+@click.pass_obj
+def get_grnodes(devices):
+    """Get appliance positions on a map for topology"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_grnodes().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_discovered')
+@click.pass_obj
+def get_discovered(devices):
+    """Returns all the discovered appliances"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_discovered().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_approved')
+@click.pass_obj
+def get_approved(devices):
+    """Returns all approved appliances"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_approved().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_interfaces')
+@click.argument('ec')
+@click.option('--cached/--no-cached', default=True)
+@click.pass_obj
+def get_interfaces(devices, ec, cached):
+    """Returns all approved appliances"""
+    for device in devices:
+        cached = str(cached).lower()
+        api = device.set_up()
+        result = api.get_interfaces(ec, cached).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+# @main.command('get_device_alarms')
+# @click.argument('ec')
+# @click.option('--view', help="all, active, closed", default="all", type=click.Choice(['all', 'active', 'closed']))
+# @click.option('--severity', help="Filters alarms by severity (warning, minor, major, critical)", default='')
+# @click.option('--order', help="Order by alarm severity (true, false)", type=click.Choice(['true', 'false']))
+# @click.option('--max-alarms', help="How many alarms to show (default=5)", default=5)
+# @click.pass_obj
+# def get_device_alarms(devices, ec, view, severity, order, max_alarms):
+#     """Returns all approved appliances"""
+#     for device in devices:
+#         api = device.set_up()
+#         result = api.get_device_alarms(ec).data
+#         print(result)
+#         outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_alarms')
+@click.option('--view', help="all, active, closed", default="all")
+@click.option('--severity', help="Filters alarms by severity (warning, minor, major, critical)")
+@click.pass_obj
+def get_alarms(devices, view, severity):
+    """Returns all approved appliances"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_alarms(view, severity).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_alarm_summary')
+@click.pass_obj
+def get_alarm_summary(devices):
+    """Returns all approved appliances"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_alarm_summary().data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
+
+@main.command('get_alarm_summary_type')
+@click.option('--alarm-type', help="Alarm Type (gms,appliance)", type=click.Choice(['gms', 'appliance']))
+@click.pass_obj
+def get_alarm_summary_type(devices, alarm_type):
+    """Alarm Type (gms,appliance)"""
+    for device in devices:
+        api = device.set_up()
+        result = api.get_alarm_summary_type(alarm_type).data
+        outFormat(device.table, result)
+        click.secho("Task Completed")
